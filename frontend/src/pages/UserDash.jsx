58b0aee0ef472c1f27dcd9557291fe7905afa78a
40149174 <adeery09@qub.ac.uk>
2023-07-15 16:33:45 +0100

    Dashboard visual edits and now only accessible to logged in users

--- frontend/src/pages/UserDash.jsx
+++ frontend/src/pages/UserDash.jsx
@@ -1,7 +1,62 @@
 import React from "react";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { Container, Row, Col, Card } from "react-bootstrap";
+import { FaSeedling } from "react-icons/fa";
+import "../index.css";
 
 function UserDash() {
-  return <div>UserDash</div>;
+  const navigate = useNavigate();
+  const { user } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    if (!user) {
+      navigate("/login");
+    }
+  }, [user, navigate]);
+
+  // We will use dummyData for now
+  const dummyData = [
+    { title: "Report 1", content: "Some content" },
+    { title: "Report 2", content: "Some other content" },
+  ];
+
+  return (
+    <>
+      <Container>
+        <Row>
+          <Col>
+            <div className="welcome-message">
+              <h1>Welcome back, Username</h1>
+            </div>
+            <div className="page-divider">
+              <FaSeedling /> <FaSeedling /> <FaSeedling />
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <div className="reports-header">
+              <h2>My Reports</h2>
+            </div>
+            {dummyData.length ? (
+              dummyData.map((report, index) => (
+                <Card className="report-card" key={index}>
+                  <Card.Body>
+                    <Card.Title>{report.title}</Card.Title>
+                    <Card.Text>{report.content}</Card.Text>
+                  </Card.Body>
+                </Card>
+              ))
+            ) : (
+              <p>You haven't created any reports yet.</p>
+            )}
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
 }
 
 export default UserDash;
