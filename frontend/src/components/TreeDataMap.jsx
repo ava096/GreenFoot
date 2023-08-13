d7fbc38cc314b4a0b25aa83a945f8dacee9c6074
40149174 <adeery09@qub.ac.uk>
2023-08-13 20:36:00 +0100

    Heatmap layer added

--- frontend/src/components/TreeDataMap.jsx
+++ frontend/src/components/TreeDataMap.jsx
@@ -4,6 +4,7 @@ import {
   useLoadScript,
   MarkerF,
   InfoWindowF,
+  HeatmapLayerF,
 } from "@react-google-maps/api";
 import LoadingSpinner from "./LoadingSpinner";
 import { Button } from "react-bootstrap";
@@ -12,17 +13,33 @@ import { useNavigate } from "react-router-dom";
 function TreeDataMap({ trees }) {
   //define navigate for use
   const navigate = useNavigate();
+  //to access heatmap
+  const GOOGLE_MAPS_LIBRARIES = ["visualization"];
 
   //set state so InfoWindow can be called when a marker is clicked
   const [selectedTree, setSelectedTree] = useState(null);
 
+  //set state to toggle heatmap on and off
+  const [showHeatmap, setShowHeatmap] = useState(false);
+
   //use API key to access maps API
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
+    //include visualisation library for heatmap
+    libraries: GOOGLE_MAPS_LIBRARIES,
   });
 
   //define the centre point for the map to automatically focus on Belfast
   const center = useMemo(() => ({ lat: 54.5973, lng: -5.9301 }), []);
+
+  //convert tree data to heatmap data as maps api expects an array of LatLng objects
+  //heatmap data will only be generated when maps api is loaded
+  const heatmapData = isLoaded
+    ? trees.map(
+        (tree) =>
+          new window.google.maps.LatLng(tree.treeLatitude, tree.treeLongitude)
+      )
+    : [];
 
   //set clicked marker to the selected tree
   const onOpenClick = (tree) => {
@@ -39,6 +56,11 @@ function TreeDataMap({ trees }) {
     navigate(`/viewTree/${selectedTree._id}`);
   };
 
+  //control heatmap display
+  const toggleHeatmap = () => {
+    setShowHeatmap(!showHeatmap);
+  };
+
   //loading state for actual TreeDataMap chil is handled in the parent
   //loading state for useLoadScript still has to be handled within child
   if (!isLoaded) {
@@ -52,33 +74,54 @@ function TreeDataMap({ trees }) {
   }
 
   return (
-    <GoogleMap zoom={10} center={center} mapContainerClassName="map-container">
-      {trees.map((tree) => (
-        <MarkerF
-          key={tree._id}
-          position={{ lat: tree.treeLatitude, lng: tree.treeLongitude }}
-          onClick={() => onOpenClick(tree)}
-        />
-      ))}
+    <>
+      <Button variant="success" onClick={toggleHeatmap}>
+        {showHeatmap ? "Hide Heatmap" : "Show Heatmap"}
+      </Button>
+      <GoogleMap
+        zoom={10}
+        center={center}
+        mapContainerClassName="map-container"
+      >
+        {!showHeatmap &&
+          trees.map((tree) => (
+            <MarkerF
+              key={tree._id}
+              position={{ lat: tree.treeLatitude, lng: tree.treeLongitude }}
+              onClick={() => onOpenClick(tree)}
+            />
+          ))}
 
-      {selectedTree && (
-        <InfoWindowF
-          position={{
-            lat: selectedTree.treeLatitude,
-            lng: selectedTree.treeLongitude,
-          }}
-          onCloseClick={onCloseClick}
-        >
-          <div>
-            <h5>{selectedTree.treeType}</h5>
-            <p>{selectedTree.treeDescription}</p>
-            <Button variant="success" onClick={onButtonClick}>
-              View Tree
-            </Button>
-          </div>
-        </InfoWindowF>
-      )}
-    </GoogleMap>
+        {selectedTree && (
+          <InfoWindowF
+            position={{
+              lat: selectedTree.treeLatitude,
+              lng: selectedTree.treeLongitude,
+            }}
+            onCloseClick={onCloseClick}
+          >
+            <div>
+              <h5>{selectedTree.treeType}</h5>
+              <p>{selectedTree.treeDescription}</p>
+              <Button variant="success" onClick={onButtonClick}>
+                View Tree
+              </Button>
+            </div>
+          </InfoWindowF>
+        )}
+
+        {showHeatmap && (
+          <HeatmapLayerF
+            data={heatmapData}
+            options={
+              {
+                // Optional heatmap options
+              }
+            }
+          />
+        )}
+      </GoogleMap>
+    </>
   );
 }
 
