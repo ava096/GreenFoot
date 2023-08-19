2fc0857eb232418b6f9bbfa4f8f1cec907f6c807
40149174 <adeery09@qub.ac.uk>
2023-08-19 14:41:35 +0100

    Page to view all reports

---
+++ frontend/src/pages/ViewAllReports.jsx
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+function ViewAllReports() {
+  return <div>ViewAllReports</div>;
+}
+
+export default ViewAllReports;
