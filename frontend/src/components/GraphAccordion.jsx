18d7c5ba4a8d5086ca3254e5e4944f74625cea59
40149174 <adeery09@qub.ac.uk>
2023-08-25 13:39:33 +0100

    Card display fixes

--- frontend/src/components/GraphAccordion.jsx
+++ frontend/src/components/GraphAccordion.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Accordion } from "react-bootstrap";
+import { Accordion, Col } from "react-bootstrap";
 import TreeCard from "./TreeCard";
 
 function GraphAccordion({ trees, categories, filterKey }) {
@@ -13,7 +13,9 @@ function GraphAccordion({ trees, categories, filterKey }) {
               {trees
                 .filter((tree) => tree[filterKey] === category)
                 .map((tree) => (
-                  <TreeCard key={tree._id} tree={tree} />
+                  <Col sm={12} md={6} lg={3} className="cardCol" key={tree._id}>
+                    <TreeCard tree={tree} />
+                  </Col>
                 ))}
             </Accordion.Body>
           </Accordion.Item>
