b823b2f7149fd7b4c06b45283229bc3e246d1b4a
40149174 <adeery09@qub.ac.uk>
2023-09-14 11:07:20 +0100

    CSS fix

--- frontend/src/components/UserFlagCard.jsx
+++ frontend/src/components/UserFlagCard.jsx
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { Card, Button } from "react-bootstrap";
 
 function UserFlagCard({ flag }) {
   const navigate = useNavigate();
+
+  const [statusMessage, setStatusMessage] = useState("");
+
+  useEffect(() => {
+    if (flag) {
+      switch (flag.flagStatus) {
+        case "pendingReview":
+          setStatusMessage("Pending Review");
+          break;
+        case "resolved":
+          setStatusMessage("Resolved");
+          break;
+        case "rejected":
+          setStatusMessage("Rejected");
+          break;
+        default:
+          setStatusMessage("Unknown");
+          break;
+      }
+    }
+  });
 
   const onViewClick = () => {
     navigate(`/viewReport/${flag.reportFlagged}`);
@@ -13,7 +34,7 @@ function UserFlagCard({ flag }) {
     <>
       <Card className="cardDisplay">
         <Card.Body>
-          <Card.Title>Status: {flag.flagStatus}</Card.Title>
+          <Card.Title>Status: {statusMessage}</Card.Title>
           <Card.Text>
             {new Date(flag.createdAt).toLocaleString("en-US")}
             <br />
